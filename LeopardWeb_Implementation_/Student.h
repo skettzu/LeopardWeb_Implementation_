fb93ac14ec30b34b45b0c93dadef867e85169254
huyphan1108 <81922720+huyphan1108@users.noreply.github.com>
2023-07-07 22:25:03 -0400

    updated student class

--- LeopardWeb_Implementation_/Student.h
+++ LeopardWeb_Implementation_/Student.h
@@ -1,25 +1,24 @@
 #pragma once
 #include "User.h"
 
+
 class Student :
 	public User {
 
 public:
 	// constructor
-
 	Student();
 	Student(string first);
 	Student(string first, string last);
 	Student(string first, string last, int ID);
-
 	// method
-	void searchCourse();
-	void addCourse();
-	void dropCourse();
+	void searchAllCourse(sqlite3* DB);
+	void searchByParameter(sqlite3* DB);
+	void addCourse(sqlite3* DB);
+	void dropCourse(sqlite3* DB);
 	void printSchedule();
 
 	//destructor
 	~Student();
-
 
 };
--- LeopardWeb_Implementation_/main.cpp
+++ LeopardWeb_Implementation_/main.cpp
@@ -168,44 +168,6 @@ int get_WID(sqlite3* LW_DB, string usr) {
 	return result;
 }
 
-void search_by_parameter(sqlite3* LW_DB) {
-	int exit = 1;
-	cout << "Enter parameter to seach course: ";
-	string user_parameter;
-	cin >> user_parameter;
-	cin.ignore();
-	//string search_parameter = ("SELECT * FROM COURSES WHERE Title = '" + user_parameter + "' OR department = '" + user_parameter + "';");
-	//string search_parameter = ("SELECT * FROM COURSES WHERE Title = '" + user_parameter + "' OR department = '" + user_parameter + "' OR day = '" + user_parameter + "' OR semester = '" + user_parameter + "' OR CRN = " + user_parameter + " OR time = " + user_parameter + " OR year = " + user_parameter + " OR credits = " + user_parameter + "; ");
-	//search for integer parameter
-	string search_parameter = ("SELECT * FROM COURSES WHERE CRN = " + user_parameter + " OR time = " + user_parameter + " OR year = " + user_parameter + " OR credits = " + user_parameter + "; ");
-	//cout << search_parameter << endl;
-	exit = sqlite3_exec(LW_DB, search_parameter.c_str(), callback, NULL, NULL);
-
-	if (exit != SQLITE_OK) {
-		//cout << "Search Error" << endl;
-		//search for string parameter
-		search_parameter = ("SELECT * FROM COURSES WHERE Title = '" + user_parameter + "' OR department = '" + user_parameter + "' OR day = '" + user_parameter + "' OR semester = '" + user_parameter + "';");
-		//cout << search_parameter << endl;
-		exit = sqlite3_exec(LW_DB, search_parameter.c_str(), callback, NULL, NULL);
-		/*
-		if (exit != SQLITE_OK) {
-			cout << "Search Error" << endl;
-		}
-		else cout << "Search Success" << endl;
-		*/
-	}
-	//else cout << "Search Success" << endl;
-}
-
-void search_all(sqlite3* LW_DB) {
-	int exit = 1;
-	string searchall = "SELECT * FROM COURSES;";
-	exit = sqlite3_exec(LW_DB, searchall.c_str(), callback, NULL, NULL);
-	if (exit != SQLITE_OK) {
-		cout << "Search Error" << endl;
-	}
-	else cout << "Search Success" << endl;
-}
 
 int main() {
 	// Database 
@@ -250,10 +212,10 @@ int main() {
 			case 3:
 				break;
 			case 4:
-				search_all(LW_DB);
+				
 				break;
 			case 5:
-				search_by_parameter(LW_DB);
+				
 				break;
 			case 6:
 				sqlite3_close(LW_DB); // close the database
@@ -306,6 +268,12 @@ int main() {
 			}
 		}
 		else if (check_class(LW_DB, username) == 3) {		// Student Menu
+			string fname = get_fname(LW_DB, username);
+			string lname = get_lname(LW_DB, username);
+			int WID = get_WID(LW_DB, username);
+			//cout << fname << " " << lname << " " << WID << endl;
+			Student student(fname, lname, WID);
+
 			cout << "1. Add Course to Semester Schedule" << endl;
 			cout << "2. Remove Course to Semester Schedule" << endl;
 			cout << "3. Search All Courses" << endl;
@@ -314,14 +282,16 @@ int main() {
 			cin >> user_input;
 			switch (user_input) {
 			case 1:
+				student.addCourse(LW_DB);
 				break;
 			case 2:
+				student.dropCourse(LW_DB);
 				break;
 			case 3:
-				search_all(LW_DB);
+				student.searchAllCourse(LW_DB);
 				break;
 			case 4:
-				search_by_parameter(LW_DB);
+				student.searchByParameter(LW_DB);
 				break;
 			case 5:
 				sqlite3_close(LW_DB); // close the database
