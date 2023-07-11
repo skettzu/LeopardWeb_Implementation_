d00d39eaeb9c4c3f7c2f8940c984fa48087fb84c
Derek Huang <huangd8@wit.edu>
2023-07-11 08:27:40 -0400

    Testing Environment

---
+++ LeopardWeb_Implementation_Test/LeopardWeb_Implementation_Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LeopardWebImplementationTest
+{
+	TEST_CLASS(LeopardWebImplementationTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
